dc18df39eda59436e654bd080eb4f2055a5155f9
yoonseoc126 <yoonseoc@usc.edu>
2024-11-25 08:39:52 -0800

    Add CommentsCountTest

---
+++ app/src/test/java/com/example/yousc/CommentsCountTest.java
@@ -0,0 +1,28 @@
+package com.example.yousc;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommentsCountTest {
+
+    /** A JUnit test method to test isqrt. */
+    @Test
+    public void testCommentsCount() {
+        List<Comment> comments = new ArrayList<>(
+                Arrays.asList(
+                        new Comment("u1@gmail.com", "11:00PM", "This is a test comment."),
+                        new Comment("u2@gmail.com", "12:00AM", "This is a test comment."),
+                        new Comment("u3@gmail.com", "1:00AM", "This is a test comment.")
+                )
+        );
+        CommentAdapter test = new CommentAdapter(comments, "user@gmail.com");
+
+        assertEquals("Comments count must match size of the comment list.", comments.size(), test.getItemCount());
+    }
+
+}
